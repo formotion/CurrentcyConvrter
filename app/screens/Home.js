d6f7ee07821b6765119f480c02414eebb96cb285
Formotion <angeluse@mail.ru>
2018-03-20 12:57:51 +0700

    move Logo

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
-import { Logo } from '../components/Container/Logo';
+import { Logo } from '../components/Logo';
 
 export default () => (
   <Container>
