7bfa9f8b742fdaefb1ea434b1ad76e3d4d483e94
Formotion <angeluse@mail.ru>
2018-04-02 18:00:24 +0700

    Options Stack Navigator

--- app/components/Header/styles.js
+++ app/components/Header/styles.js
@@ -1,5 +1,4 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { StatusBar } from 'react-native';
 
 const styles = EStyleSheet.create({
   container: {
@@ -8,7 +7,6 @@ const styles = EStyleSheet.create({
     right: 0,
     top: 0,
     '@media ios': { paddingTop: 20 },
-    '@media android': { paddingTop: StatusBar.currentHeight },
   },
   button: {
     alignSelf: 'flex-end',
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { StatusBar, KeyboardAvoidingView } from 'react-native';
 
 import { Container } from '../components/Container';
@@ -16,11 +17,14 @@ const TEMP_CONVERSION_RATE = 0.7974;
 const TEMP_LAST_CONVERTED = new Date();
 
 class Home extends Component {
+  static propTypes = {
+    navigation: PropTypes.object,
+  };
   handlePressBaseCurrency = () => {
-    console.log('Press base');
+    this.props.navigation.navigate('CurrencyList', { title: 'Base Currency' });
   };
   handlePressQuoteCurrency = () => {
-    console.log('Press quote');
+    this.props.navigation.navigate('CurrencyList', { title: 'Quote Currency' });
   };
 
   handleChangeText = (text) => {
