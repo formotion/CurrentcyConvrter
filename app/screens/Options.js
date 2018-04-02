f21693451eb14974edcce7e2e33e613ec330913d
Formotion <angeluse@mail.ru>
2018-04-02 14:45:53 +0700

    Options and icon

---
+++ app/screens/Options.js
@@ -0,0 +1,38 @@
+import { Ionicons } from '@expo/vector-icons';
+import React, { Component } from 'react';
+import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ListItem, Separator } from '../components/List';
+
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
+
+class Options extends Component {
+  handleThemesPress = () => {
+    console.log('press themes');
+  };
+  handleSitePress = () => {
+    console.log('press Site');
+  };
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar translucent={false} barStyle="default" />
+        <ListItem
+          text="Themes"
+          onPress={this.handleThemesPress}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-arrow-forward`} color={ICON_COLOR} size={ICON_SIZE} />
+          }
+        />
+        <Separator />
+        <ListItem
+          text="Fixer.io"
+          onPress={this.handleSitePress}
+          customIcon={<Ionicons name={`${ICON_PREFIX}-link`} color={ICON_COLOR} size={ICON_SIZE} />}
+        />
+      </ScrollView>
+    );
+  }
+}
+export default Options;
