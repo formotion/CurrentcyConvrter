b875f4e09c5bc6083eb4bf69be69a7642a98fe3e
Formotion <angeluse@mail.ru>
2018-04-16 14:59:39 +0700

    Api

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -1,20 +1,66 @@
 import {
-  CHANGE_CURRENCY_AMOUNT,
   SWAP_CURRENCY,
+  CHANGE_CURRENCY_AMOUNT,
   CHANGE_BASE_CURRENCY,
   CHANGE_QUOTE_CURRENCY,
-  GET_INITIAL_CONVERSION,
   CONVERSION_RESULT,
   CONVERSION_ERROR,
+  GET_INITIAL_CONVERSION,
 } from '../actions/currencies';
 
 const initialState = {
-  baseCurrency: 'USD',
+  baseCurrency: 'EUR',
   quoteCurrency: 'GBP',
   amount: 100,
   conversions: {},
   error: null,
 };
+
+// const initialState = {
+//   baseCurrency: 'USD',
+//   quoteCurrency: 'GBP',
+//   amount: 100,
+//   conversions: {
+//     USD: {
+//       isFetching: false,
+//       base: 'USD',
+//       date: '2017-05-31',
+//       rates: {
+//         AUD: 1.3416,
+//         BGN: 1.743,
+//         BRL: 3.2515,
+//         CAD: 1.3464,
+//         CHF: 0.97104,
+//         CNY: 6.813,
+//         CZK: 23.547,
+//         DKK: 6.6302,
+//         GBP: 0.77858,
+//         HKD: 7.7908,
+//         HRK: 6.6068,
+//         HUF: 273.77,
+//         IDR: 13308,
+//         ILS: 3.5431,
+//         INR: 64.463,
+//         JPY: 110.86,
+//         KRW: 1118.4,
+//         MXN: 18.765,
+//         MYR: 4.281,
+//         NOK: 8.4117,
+//         NZD: 1.4071,
+//         PHP: 49.77,
+//         PLN: 3.7173,
+//         RON: 4.0687,
+//         RUB: 56.774,
+//         SEK: 8.6942,
+//         SGD: 1.3829,
+//         THB: 34.07,
+//         TRY: 3.5366,
+//         ZAR: 13.133,
+//         EUR: 0.89119,
+//       },
+//     },
+//   },
+// };
 
 const setConversions = (state, action) => {
   let conversion = {
@@ -22,7 +68,6 @@ const setConversions = (state, action) => {
     date: '',
     rates: {},
   };
-
   if (state.conversions[action.currency]) {
     conversion = state.conversions[action.currency];
   }
@@ -33,10 +78,13 @@ const setConversions = (state, action) => {
   };
 };
 
-export default (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
-      return { ...state, amount: action.amount || 0 };
+      return {
+        ...state,
+        amount: action.amount || 0,
+      };
     case SWAP_CURRENCY:
       return {
         ...state,
@@ -47,30 +95,37 @@ export default (state = initialState, action) => {
       return {
         ...state,
         baseCurrency: action.currency,
-        conversions: setConversions(state, action),
+        conversion: setConversions(state, action),
       };
     case CHANGE_QUOTE_CURRENCY:
       return {
         ...state,
         quoteCurrency: action.currency,
+        conversion: setConversions(state, action),
       };
     case GET_INITIAL_CONVERSION:
-      return { ...state, conversions: setConversions(state, { currency: state.baseCurrency }) };
+      return {
+        ...state,
+        conversion: setConversions(state, { currency: state.baseCurrency }),
+      };
     case CONVERSION_RESULT:
       return {
         ...state,
         baseCurrency: action.result.base,
         conversions: {
           ...state.conversions,
-          [action.result.base]: {
-            isFetching: false,
-            ...action.result,
-          },
+          [action.result.base]: { isFetching: false, ...action.result },
         },
       };
     case CONVERSION_ERROR:
-      return { ...state, error: action.error };
+      return {
+        ...state,
+        error: action.error,
+      };
+
     default:
       return state;
   }
 };
+
+export default reducer;
