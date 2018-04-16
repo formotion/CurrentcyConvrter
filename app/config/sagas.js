b4222acf7ccd7e17de89683a9ad6713b91ef5b6d
Formotion <angeluse@mail.ru>
2018-04-16 10:48:34 +0700

    Configuring Redux Saga

---
+++ app/config/sagas.js
@@ -0,0 +1,13 @@
+import { takeEvery } from 'redux-saga/effects';
+
+import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies';
+
+function* fetchLatestConversionRates(action) {
+  console.log('TODO: Update thi things ', action);
+  yield;
+}
+export default function* rootSaga() {
+  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
+  yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
+  yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
+}
--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 
 import reducers from '../reducers';
+import rootSaga from './sagas';
 
-const middleware = [];
+const SagaMiddleware = createSagaMiddleware();
+const middleware = [SagaMiddleware];
 if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 
-export default createStore(reducers, applyMiddleware(...middleware));
+const store = createStore(reducers, applyMiddleware(...middleware));
+
+SagaMiddleware.run(rootSaga);
+
+export default store;
